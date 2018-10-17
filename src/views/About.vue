9a8181b777e6d8491ab5cbdee6dd9089be38bbae
Copelia <iamcorallium@gmail.com>
2018-10-17 00:06:43 -0500

    Añadiendo vista About me y Contact con algunas etiquetas

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="../assets/corali.png" alt="copelia descansando">
+      <div class="card-body">
+        <p class="card-text">Me gusta subir al bosque en dos ruedas</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+
+
+</style>
+
