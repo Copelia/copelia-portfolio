548141230018e620b66b246a98592e745a51ae38
Copelia <iamcorallium@gmail.com>
2018-10-19 11:17:53 -0500

    Mejorando estilo de foto

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="../assets/corali.png" alt="copelia descansando">
-      <div class="card-body">
-        <p class="card-text">Me gusta subir al bosque en dos ruedas</p>
-      </div>
+    <div class="bio-pic">
+      <img class="rounded-circle" src="../assets/corali.png" alt="copelia descansando">
     </div>
+        <p class="card-text">Me gusta ir al bosque en dos ruedas</p>
   </div>
 </template>
 
 <style>
+img{
+  width: 300px;
+  height: 300px;
+}
 
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <Hello msg="Front-end Developer" profile="Copelia Altamirano"/>
   </div>
 </template>
