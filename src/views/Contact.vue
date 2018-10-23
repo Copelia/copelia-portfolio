e09d4d9c35b176eb5c2f63f948ffb4f0d01c3bf0
Copelia <iamcorallium@gmail.com>
2018-10-23 12:05:10 -0500

    Acomodando iconos contacto

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,13 +2,13 @@
   <div class="container">
     <div class="row justify-center">
       <div class="col-12">
-        <i class="fab fa-github"></i>
+        <i class="fab fa-github pair"></i>
         <i class="fab fa-linkedin"></i>
-        <i class="fas fa-envelope"></i>
-      </div>
-      <div class="col-12">  
+        <i class="fas fa-envelope pair"></i>
+      <!-- </div>
+      <div class="col-12">   -->
         <i class="fab fa-dev"></i>
-        <i class="far fa-file"></i>
+        <i class="far fa-file pair"></i>
       </div>
     </div>
   </div>
@@ -16,7 +16,14 @@
 
 <style>
 i{
-  width: 10vh;
-  height: 10vh;
+  margin: 10px 10px 10px 10px;
+  font-size: 5em;
+  color: #64AF76;
+  /* text-align: center !important; */
 }
+
+.pair{
+  color: #419B7F;
+}
+
 </style>
