6c0a1cc7ac4b4f82406bce02a05f7b8f66f751ee
Copelia <iamcorallium@gmail.com>
2018-11-09 14:00:39 -0600

    Modificando links para responsive

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div class="nav" id="nav" style="font-family: 'Cabin Sketch'">
-      <router-link to="/">Inicio</router-link> |
-      <router-link to="/about">¿Quién?</router-link> |
-      <router-link to="/contact">¿Dónde?</router-link>
+    <div class="nav">
+      <router-link to="/">Inicio  |</router-link>
+      <router-link to="/about">  ¿Quién?  |</router-link>
+      <router-link to="/contact">  ¿Dónde?</router-link>
     </div>
     <div class="header">
       <h4>Copelia Altamirano</h4>
@@ -13,24 +13,36 @@
   </div>
 </template>
 
-<style lang="stylus">
-.nav
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align right
-  color #2c3e50
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    font-size 2em
-  .router-link-exact-active
-    color #42b983
-    
+<style>
+.nav{
+  font-family: 'Cabin Sketch';
+  text-align: right;
+  color: #42b983;
+  padding: 30px;
+}
+  a{
+    font-weight: bold;
+    color: #42b983;
+    font-size: 3vw;
+  }
+  .router-link-exact-active{
+    color: #2c3e50;
+  }
+
  .header{
      text-align: right;
      margin-bottom: 0.5em;
-     font-size: 4em;
-     color: #373D3F;
-  }  
+     font-size: 2vw;
+     color: #2c3e50;
+  }
+
+  @media screen and (max-width: 375px) {
+  a{
+    font-size: 20px;
+  }
+
+  .header{
+    font-size: 15px;
+  }
+}
 </style>
